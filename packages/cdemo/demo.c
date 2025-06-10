6176431f7573e3db1e0e24b6b056e3ee3010ad79
TheSola10 <me@thesola.io>
2025-06-10 23:01:15 +0200

    Fixed event handling in CDemo

--- packages/cdemo/demo.c
+++ packages/cdemo/demo.c
@@ -5,17 +5,26 @@ typedef long unsigned int time_t;
 
 void *closure;
 
+void btnClicked(void *evt)
+{
+    ELJApp_Exit();
+}
+
 void app_main(void *dat)
 {
-    void *fr = wxFrame_Create(NULL, 1000, "wxKitchen demo written in C", 40, 40, 320, 240, wxDEFAULT_FRAME_STYLE);
+    void *fr = wxFrame_Create(NULL, 10, "wxKitchen demo written in C", 40, 40, 320, 240, wxDEFAULT_FRAME_STYLE);
 
-    void *pan = wxPanel_Create(fr, 1100, 0, 0, 320, 240, NULL);
+    void *pan = wxPanel_Create(fr, 11, 0, 0, 320, 240, NULL);
 
-    void *bt = wxButton_Create(pan, 1200, "Nice", 40, 40, 100, 20, wxBU_EXACTFIT);
+    void *bt = wxButton_Create(pan, 12, "Nice", 40, 40, 100, 20, wxBU_EXACTFIT);
+
+    void *evt = wxClosure_Create(btnClicked, NULL);
 
     wxWindow_Show(fr);
 
     ELJApp_SetTopWindow(fr);
+
+    wxEvtHandler_Connect(fr, 12, 12, expEVT_COMMAND_BUTTON_CLICKED(), evt);
 }
 
 int main(int argc, char *argv[])
