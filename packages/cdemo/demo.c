f4ba8f0fc4d439ada7ac183fc7ae9004d2fe3d59
TheSola10 <me@thesola.io>
2025-06-11 15:30:59 +0200

    trying out Sizer

--- packages/cdemo/demo.c
+++ packages/cdemo/demo.c
@@ -17,13 +17,22 @@ void app_main(void *dat)
     wxClosure *evt;
 
     fr = wxFrame_Create(NULL, 10, "wxKitchen demo written in C", 40, 40, 320, 240, wxDEFAULT_FRAME_STYLE);
-    pan = wxPanel_Create((wxWindow *) fr, 11, 0, 0, 320, 240, NULL);
+    pan = wxPanel_Create((wxWindow *) fr, 11, 0, 0, 320, 240, wxTAB_TRAVERSAL);
     bt = wxButton_Create((wxWindow *) pan, 12, "Nice", 40, 40, 100, 20, wxBU_EXACTFIT);
     evt = wxClosure_Create(btnClicked, NULL);
 
-    wxBoxSizer_Create(wxVERTICAL);
+    wxBoxSizer *parent = wxBoxSizer_Create(wxVERTICAL);
+    wxBoxSizer *body = wxBoxSizer_Create(wxHORIZONTAL);
+    wxBoxSizer *btns = wxBoxSizer_Create(wxHORIZONTAL);
+
+    wxSizer_AddSizer((wxSizer *) parent, (wxSizer *) body, 1, wxEXPAND, wxALL, NULL);
+    wxSizer_AddSizer((wxSizer *) parent, (wxSizer *) btns, 0, wxALIGN_RIGHT | wxRIGHT | wxBOTTOM, 10, NULL);
+
+    wxSizer_AddWindow((wxSizer *) btns, (wxWindow *) bt, 0, 0, wxALL, NULL);
 
     wxEvtHandler_Connect((wxEvtHandler *) fr, 12, 12, expEVT_COMMAND_BUTTON_CLICKED(), evt);
+
+    wxWindow_SetSizer((wxWindow *)pan, (wxSizer *) parent);
 
     wxWindow_Show((wxWindow *) fr);
     ELJApp_SetTopWindow((wxWindow *) fr);
