59280dec4fc02d2a7a02e60da1da8eb8a17d6b25
TheSola10 <me@thesola.io>
2025-06-11 13:51:15 +0200

    style updates for CDemo

--- packages/cdemo/demo.c
+++ packages/cdemo/demo.c
@@ -11,25 +11,23 @@ void btnClicked(void *evt)
 
 void app_main(void *dat)
 {
-    TClass(wxFrame) fr = wxFrame_Create(NULL, 10, "wxKitchen demo written in C", 40, 40, 320, 240, wxDEFAULT_FRAME_STYLE);
+    wxFrame *fr;
+    wxPanel *pan;
+    wxButton *bt;
+    wxClosure *evt;
 
-    TClass(wxPanel) pan = wxPanel_Create((TClass(wxWindow)) fr, 11, 0, 0, 320, 240, NULL);
+    fr = wxFrame_Create(NULL, 10, "wxKitchen demo written in C", 40, 40, 320, 240, wxDEFAULT_FRAME_STYLE);
+    pan = wxPanel_Create((wxWindow *) fr, 11, 0, 0, 320, 240, NULL);
+    bt = wxButton_Create((wxWindow *) pan, 12, "Nice", 40, 40, 100, 20, wxBU_EXACTFIT);
+    evt = wxClosure_Create(btnClicked, NULL);
 
-    TClass(wxButton) bt = wxButton_Create((TClass(wxWindow)) pan, 12, "Nice", 40, 40, 100, 20, wxBU_EXACTFIT);
+    wxEvtHandler_Connect((wxEvtHandler *) fr, 12, 12, expEVT_COMMAND_BUTTON_CLICKED(), evt);
 
-    TClass(wxClosure) evt = wxClosure_Create(btnClicked, NULL);
-
-    wxWindow_Show((TClass(wxWindow)) fr);
-
-    ELJApp_SetTopWindow((TClass(wxWindow)) fr);
-
-    wxEvtHandler_Connect((TClass(wxEvtHandler)) fr,
-            12, 12, expEVT_COMMAND_BUTTON_CLICKED(), evt);
+    wxWindow_Show((wxWindow *) fr);
+    ELJApp_SetTopWindow((wxWindow *) fr);
 }
 
 int main(int argc, char *argv[])
 {
-    TClass(wxClosure) closure = wxClosure_Create(app_main, NULL);
-
-    ELJApp_InitializeC(closure, argc, argv);
+    ELJApp_InitializeC(wxClosure_Create(app_main, NULL), argc, argv);
 }
